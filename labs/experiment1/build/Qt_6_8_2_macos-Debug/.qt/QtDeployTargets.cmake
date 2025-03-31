fed8742aac3cf9ed0fadd6ebdc301dd4b05fe794
artem <artem@MacBook-Pro-vladik.local>
2025-03-31 18:23:07 +0300

    task2

---
+++ labs/experiment1/build/Qt_6_8_2_macos-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_experiment1_FILE /Users/artem/Documents/Other/experiment1/build/Qt_6_8_2_macos-Debug/experiment1.app/Contents/MacOS/experiment1)
+set(__QT_DEPLOY_TARGET_experiment1_TYPE EXECUTABLE)
---
+++ labs/experiment1/build/Qt_6_8_2_macos-Debug/CMakeFiles/experiment1.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/experiment1_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/experiment1_autogen.dir/ParseCache.txt"
+  "experiment1_autogen"
+  "CMakeFiles/experiment1.dir/experiment1_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/experiment1.dir/experiment1_autogen/mocs_compilation.cpp.o.d"
+  "CMakeFiles/experiment1.dir/main.cpp.o"
+  "CMakeFiles/experiment1.dir/main.cpp.o.d"
+  "CMakeFiles/experiment1.dir/mainwindow.cpp.o"
+  "CMakeFiles/experiment1.dir/mainwindow.cpp.o.d"
+  "experiment1.app/Contents/MacOS/experiment1"
+  "experiment1.pdb"
+  "experiment1_autogen/mocs_compilation.cpp"
+  "experiment1_autogen/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/experiment1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
