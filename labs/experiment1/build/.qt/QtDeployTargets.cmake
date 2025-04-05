68a4b42439326ae43f5978a4143dec01d8929394
ArtemAbramchik <41artem41@gmail.com>
2025-04-05 21:14:19 +0300

    Add task2

--- labs/experiment1/build/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_experiment1_FILE /Users/artem/Documents/Other/experiment1/build/experiment1.app/Contents/MacOS/experiment1)
-set(__QT_DEPLOY_TARGET_experiment1_TYPE EXECUTABLE)
--- labs/experiment1/build/Qt_6_8_2_macos-Debug/CMakeFiles/experiment1.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for experiment1.
-# This may be replaced when dependencies are built.
